38e7ec0b9259de532e65a4cf5cbff2ec5fababc4
xiaojin <admin@iothn.cn>
2025-11-28 14:24:54 +0800

    修改 config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rudesk.iothn.cn"];
+pub const RS_PUB_KEY: &str = "8yYw9Klmr8QHHZ+TegxDesJZzbpNZLuLELyA1gv4pQk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
